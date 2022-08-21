6b3327e7fdb7ee3408683f5c886d415b4e744e8e
Sepehr <bazyar.sepehr@gmail.com>
2022-08-21 14:12:28 +0430

    feat: write decorator to process time of functions

---
+++ decorators.py
@@ -0,0 +1,23 @@
+from functools import wraps
+from typing import Callable
+from timeit import default_timer as timer
+
+
+def process_timer(function: Callable[[list], list]) -> Callable[[list], list]:
+    """decorator for processing timer sorting algorithm functions
+
+    :param function: function of sorting algorithms
+    :type function: Callable[[list], list]
+    :return: decorated function of sorting algorithms with process time
+    :rtype: Callable[[list], list]
+    """
+
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        start_time = timer()
+        result = function(*args, **kwargs)
+        process_time = (timer() - start_time) * 1_000_000  # convert to seconds
+        print(f"{function.__name__}[{len(result)}]: {process_time}s")
+        return result
+
+    return wrapper
