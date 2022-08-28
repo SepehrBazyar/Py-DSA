4d1c46c7e09cc34cd4a0495218ba886b7aabf477
Sepehr <bazyar.sepehr@gmail.com>
2022-08-28 11:44:55 +0430

    refactor: two pointer on first and len of numbers

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -17,7 +17,7 @@ class Queue:
         :param length: the maximum length size of this queue
         :type length: int
         """
-        self.__length, self.__front, self.__rear = length, 0, 0
+        self.__length, self.__first, self.__number = length, 0, 0
         self.__queue: List[Optional[int]] = [None for _ in range(self.__length)]
 
     def dequeue(self) -> int:
@@ -30,11 +30,12 @@ class Queue:
         :return: the value to be popped
         :rtype: int
         """
-        if self.__front == self.__rear:
+        if self.__number == 0:
             raise UnderflowError("Queue is empty.")
 
-        item, self.__queue[self.__front] = self.__queue[self.__front], None
-        self.__front += 1
+        item, self.__queue[self.__first] = self.__queue[self.__first], None
+        self.__first = (self.__first + 1) % self.__length
+        self.__number -= 1
         return item
 
     def enqueue(self, value: int):
@@ -46,11 +47,11 @@ class Queue:
         :param value: the value to be pushed
         :type value: int
         """
-        if self.__rear == self.__length:
+        if self.__number >= self.__length:
             raise OverflowError("Queue is full.")
 
-        self.__queue[self.__rear] = value
-        self.__rear += 1
+        self.__queue[(self.__number + self.__first) % self.__length] = value
+        self.__number += 1
 
     def __repr__(self) -> str:
         """
@@ -59,5 +60,8 @@ class Queue:
         :return: representation string
         :rtype: str
         """
-        items = (str(elem) for elem in self.__queue[::-1] if elem is not None)
-        return f"-> _ {' | '.join(items)} ->"
+        items = []
+        for index in range(self.__number):
+            items.append(str(self.__queue[(index + self.__first) % self.__length]))
+
+        return f"-> _ {' | '.join(reversed(items))} ->"
