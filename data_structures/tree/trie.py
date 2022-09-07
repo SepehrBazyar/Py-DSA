30830237d474aa99c308169f9927696cb4b23e98
Sepehr <bazyar.sepehr@gmail.com>
2022-09-07 17:35:59 +0430

    feat: write search and delete methods of trie

--- data_structures/tree/trie.py
+++ data_structures/tree/trie.py
@@ -28,6 +28,26 @@ class Trie:
         """
         return self._insert(string=string, node=self.__root)
 
+    def search(self, string: str) -> bool:
+        """
+        Search a string into the Trie.
+
+        :param string: string to search into the Trie
+        :type string: str
+        :return: boolean result of search string in trie
+        :rtype: _type_
+        """
+        return self._search(string=string, node=self.__root)
+
+    def delete(self, string: str):
+        """
+        Delete a string from the Trie.
+
+        :param string: string to delete from the Trie
+        :type string: str
+        """
+        return self._delete(string=string, node=self.__root)
+
     def _insert(self, *, string: str, node: "_Node", index: int = 0) -> "_Node":
         """
         Protected Recursive Insert Utility Method in trie.
@@ -46,15 +66,71 @@ class Trie:
             return node
 
         char = string[index]
-        for edge in node.edges:
-            if edge.label == char:
-                next_node = edge.end
-                break
-        else:
+        next_node = self.__get(character=char, node=node)
+        if next_node is None:
             next_node = _Node(parent=node)
             node.edges.append(_Edge(label=char, start=node, end=next_node))
 
         return self._insert(string=string, node=next_node, index=index + 1)
+
+    def _search(self, *, string: str, node: "_Node", index: int = 0) -> bool:
+        """
+        Protected Recursive Search Utility Method in trie.
+
+        :param string: string to search the trie
+        :type string: str
+        :param node: root node to search and mark
+        :type node: _Node
+        :param index: index number of string character, defaults to 0
+        :type index: int, optional
+        :return: boolean result of search string in trie
+        :rtype: bool
+        """
+        if index == len(string):  # base case
+            return node.is_end
+
+        char = string[index]
+        next_node = self.__get(character=char, node=node)
+        if next_node is None:
+            return False
+
+        return self._search(string=string, node=next_node, index=index + 1)
+
+    def _delete(self, *, string: str, node: "_Node", index: int = 0) -> None:
+        """
+        Protected Recursive Delete Utility Method in trie.
+
+        :param string: string to search the trie
+        :type string: str
+        :param node: root node to search and mark
+        :type node: _Node
+        :param index: index number of string character, defaults to 0
+        :type index: int, optional
+        :raises ValueError: if not find this string item in trie.
+        :return: boolean result of search string in trie
+        :rtype: bool
+        """
+        if index == len(string):  # base case
+            node.unset_mark()
+            return
+
+        char = string[index]
+        next_node = self.__get(character=char, node=node)
+        if next_node is None:
+            raise ValueError("Could not find this string item in trie.")
+
+        return self._delete(string=string, node=next_node, index=index + 1)
+
+    def __get(self, character: str, node: "_Node") -> Optional["_Node"]:
+        """
+        Private method for getting the node has this character label.
+
+        :return: returned node object if exist and find values otherwise None
+        :rtype: Optional[_Node]
+        """
+        for edge in node.edges:
+            if edge.label == character:
+                return edge.end
 
 
 class _Node:
@@ -70,6 +146,17 @@ class _Node:
         """
         self.is_end, self.parent = mark, parent
         self.edges: List[_Edge] = []
+
+    def unset_mark(self):
+        """
+        Unset mark flag for end of words.
+
+        :raises Exception: if mark flag is false
+        """
+        if not self.is_end:
+            raise Exception("Node already isn't end of words.") 
+
+        self.is_end = False
 
 
 @dataclass
