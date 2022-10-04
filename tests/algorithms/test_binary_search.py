c274dcf80e57054f558bc62d66c129f80c60c5e9
Sepehr <bazyar.sepehr@gmail.com>
2022-10-04 13:55:31 +0330

    test: write new tests for quick select function

--- tests/algorithms/test_binary_search.py
+++ tests/algorithms/test_binary_search.py
@@ -1,13 +1,13 @@
 from pytest import mark, raises
 
 from algorithms import binary_search
-from ..conftest import SORTED_ODD_ARRAY, SORTED_EVEN_ARRAY
+from ..conftest import ODD_ARRAY, EVEN_ARRAY
 
 
 @mark.parametrize(
     "item, array, expected",
-    [(item, SORTED_EVEN_ARRAY, index) for index, item in enumerate(SORTED_EVEN_ARRAY)]
-    + [(item, SORTED_ODD_ARRAY, index) for index, item in enumerate(SORTED_ODD_ARRAY)],
+    [(item, EVEN_ARRAY.sorted, index) for index, item in enumerate(EVEN_ARRAY.sorted)]
+    + [(item, ODD_ARRAY.sorted, index) for index, item in enumerate(ODD_ARRAY.sorted)],
 )
 def test_binary_search(item, array, expected):
     assert binary_search(item, array) == expected
@@ -15,4 +15,4 @@ def test_binary_search(item, array, expected):
 
 def test_binary_search_errors():
     with raises(ValueError):
-        binary_search(10, SORTED_ODD_ARRAY)
+        binary_search(10, ODD_ARRAY.sorted)
