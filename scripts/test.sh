77e63cc7b8dc36582e40f38658214c5b52bf56e2
Sepehr <bazyar.sepehr@gmail.com>
2022-10-07 14:06:43 +0330

    feat: added bash script for execute tests

---
+++ scripts/test.sh
@@ -0,0 +1,8 @@
+#!bin/bash -e
+
+set -x
+
+ALGORITHMS="algorithms"
+DATA_STRUCTURES="data_structures"
+
+pytest --cov=${ALGORITHMS} --cov=${DATA_STRUCTURES} --cov-fail-under=100 --cov-report=term-missing
